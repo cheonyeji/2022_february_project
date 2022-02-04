26d41750b527cef17fbef5f7e656d5abe7515b37
1000yj <37067204+cheonyeji@users.noreply.github.com>
2022-02-05 02:10:15 +0900

    Add theme

---
+++ todo-diary/src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  bgColor: "#f5f6fa",
+  textColor: "2f3640",
+  accentColor: "#9c88ff",
+  listItemColor: "#8c7ae6",
+};
