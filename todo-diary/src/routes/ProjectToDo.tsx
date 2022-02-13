fecb68e24369e52a72a95940cb560c9cc96d402b
1000yj <37067204+cheonyeji@users.noreply.github.com>
2022-02-13 12:12:29 +0900

    project 클릭시 보여지는 화면
    (중복 요소가 많아 리팩토링 필요)

---
+++ todo-diary/src/routes/ProjectToDo.tsx
@@ -0,0 +1,62 @@
+import { Helmet } from "react-helmet";
+import { useParams } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import styled from "styled-components";
+import { clickedTabState, toDoSelector } from "../atoms";
+import SideBar from "../pages/SideBar/SideBar";
+import NewToDo from "../pages/ToDo/NewToDo";
+import ToDo from "../pages/ToDo/ToDo";
+
+const Container = styled.div`
+  padding: 0px 20px;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 15px;
+`;
+
+const ToDoWrapper = styled.div`
+  grid-column: 2/6;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  padding: 10px 0;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  font-weight: Bold;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const TodoOl = styled.ol``;
+
+interface RouteParams {
+  projectId: string;
+}
+
+function ProjectToDo() {
+  const toDos = useRecoilValue(toDoSelector);
+  const { projectId } = useParams<RouteParams>();
+  return (
+    <Container>
+      <Helmet>
+        <title>To Do w/ Diary</title>
+      </Helmet>
+      <SideBar />
+      <ToDoWrapper>
+        <Header>
+          <Title>{projectId}</Title>
+        </Header>
+        <TodoOl>
+          {toDos.map((toDo) => (
+            <ToDo key={toDo.id} {...toDo} />
+          ))}
+        </TodoOl>
+        <NewToDo />
+      </ToDoWrapper>
+    </Container>
+  );
+}
+
+export default ProjectToDo;
