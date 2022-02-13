7fc93e5b26feec1dc0cad073b12c8a5d13a005bd
1000yj <37067204+cheonyeji@users.noreply.github.com>
2022-02-13 12:08:57 +0900

    삭제후 홈이동, Btn 추가
    - StatusBtn : component로 추후 이동 가능

--- todo-diary/src/pages/SideBar/Category.tsx
+++ todo-diary/src/pages/SideBar/Category.tsx
@@ -1,19 +1,42 @@
 import { Link } from "react-router-dom";
-import { useSetRecoilState } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import styled from "styled-components";
-import { categoryState, ICategory } from "../../atoms";
+import {
+  categoryState,
+  clickedTabState,
+  ICategory,
+  selectedCategoryState,
+  Statuses,
+  toDoState,
+} from "../../atoms";
 
 const CategoryLi = styled.li`
   padding: 10px 0;
+  display: flex;
 `;
 
 const DeleteBtn = styled.button`
   background-color: transparent;
   border: none;
 `;
-
+const StatusBtn = styled.button<{ isActive?: boolean }>`
+  height: 100%;
+  width: 100%;
+  display: block;
+  border: none;
+  cursor: pointer;
+  text-align: left;
+  padding: 10px 0px;
+  margin-bottom: 20px;
+  background-color: transparent;
+  color: ${(props) =>
+    props.isActive ? props.theme.accentColor : props.theme.textColor};
+`;
 function Category({ id, text }: ICategory) {
+  const setSelectedCategory = useSetRecoilState(selectedCategoryState);
   const setCategories = useSetRecoilState(categoryState);
+  const setToDos = useSetRecoilState(toDoState);
+  const setClickedTab = useSetRecoilState(clickedTabState);
   const handleDelete = () => {
     setCategories((prevCategories) => {
       const targetIndex = prevCategories.findIndex(
@@ -24,13 +47,28 @@ function Category({ id, text }: ICategory) {
         ...prevCategories.slice(targetIndex + 1),
       ];
     });
+    setToDos((prevToDos) => prevToDos.filter((toDo) => toDo.category !== text));
+    setClickedTab({ category: "", status: Statuses.DOING });
+    setSelectedCategory("할일");
+  };
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    setClickedTab({ category: name as any, status: Statuses.TO_DO });
   };
   return (
     <CategoryLi>
-      <Link to={`/${text}`}>
-        <span>{text}</span>
+      <Link to={`/projectId/${text}`}>
+        <StatusBtn name={text} onClick={onClick}>
+          <span>{text}</span>
+        </StatusBtn>
       </Link>
-      {text !== "할일" ? <DeleteBtn onClick={handleDelete}>✖</DeleteBtn> : null}
+      {text !== "할일" ? (
+        <DeleteBtn onClick={handleDelete}>
+          <Link to={`/status/${Statuses.DOING.toLowerCase()}`}>✖</Link>
+        </DeleteBtn>
+      ) : null}
     </CategoryLi>
   );
 }
