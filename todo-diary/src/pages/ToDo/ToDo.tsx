6fb32c81a7d3183a7cd3e4d8ab84968f3a1a768e
1000yj <37067204+cheonyeji@users.noreply.github.com>
2022-02-13 12:11:11 +0900

    우클릭메뉴로 데드라인 설정&프젝이동

--- todo-diary/src/pages/ToDo/ToDo.tsx
+++ todo-diary/src/pages/ToDo/ToDo.tsx
@@ -1,7 +1,17 @@
 import React from "react";
-import { useSetRecoilState } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import styled from "styled-components";
-import { IToDo, Statuses, toDoState } from "../../atoms";
+import { categoryState, IToDo, Statuses, toDoState } from "../../atoms";
+import {
+  Menu,
+  Item,
+  Separator,
+  Submenu,
+  useContextMenu,
+  ItemParams,
+  ItemProps,
+} from "react-contexify";
+import "react-contexify/dist/ReactContexify.css";
 
 const ToDoLi = styled.li`
   background-color: ${(props) => props.theme.listItemColor};
@@ -17,8 +27,14 @@ const DeleteBtn = styled.button`
   background-color: transparent;
   border: none;
 `;
+
+const MENU_ID = "SET_PROPERTY";
+// Defined just for documentation purpose
+type ItemData = any;
+
 function ToDo({ id, title, status, category }: IToDo) {
   const setToDos = useSetRecoilState(toDoState);
+  const categories = useRecoilValue(categoryState);
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { checked } = event.target;
     // 안한거로 변경
@@ -54,8 +70,55 @@ function ToDo({ id, title, status, category }: IToDo) {
       return finalToDos;
     });
   };
+
+  const { show } = useContextMenu({ id: MENU_ID });
+  const handleItemClick = ({ event }: ItemParams<ItemProps, ItemData>) => {
+    switch (event.currentTarget.id) {
+      case Statuses.TO_DO:
+        setToDos((oldToDos) => {
+          const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+          return [
+            ...oldToDos.slice(0, targetIndex),
+            { title, id, status: Statuses.TO_DO, category },
+            ...oldToDos.slice(targetIndex + 1),
+          ];
+        });
+        break;
+      case Statuses.DOING:
+        setToDos((oldToDos) => {
+          const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+          return [
+            ...oldToDos.slice(0, targetIndex),
+            { title, id, status: Statuses.DOING, category },
+            ...oldToDos.slice(targetIndex + 1),
+          ];
+        });
+        break;
+      case Statuses.DONE:
+        setToDos((oldToDos) => {
+          const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+          return [
+            ...oldToDos.slice(0, targetIndex),
+            { title, id, status: Statuses.DONE, category },
+            ...oldToDos.slice(targetIndex + 1),
+          ];
+        });
+        break;
+    }
+  };
+
+  const handleCategoryClick = ({ event }: ItemParams<ItemProps, ItemData>) => {
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        { title, id, status, category: event.currentTarget.innerText },
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
+  };
   return (
-    <ToDoLi>
+    <ToDoLi onContextMenu={show}>
       <input
         type="checkbox"
         checked={status !== Statuses.DONE ? false : true}
@@ -63,9 +126,29 @@ function ToDo({ id, title, status, category }: IToDo) {
         onChange={onChange}
       />
       <span>
-        {title} / 카테고리 : {category}
+        {title} (프로젝트 : {category})
       </span>
       <DeleteBtn onClick={handleDelete}>❌</DeleteBtn>
+
+      <Menu id={MENU_ID}>
+        <Item onClick={handleItemClick} id={Statuses.DOING}>
+          기한: 오늘
+        </Item>
+        <Item onClick={handleItemClick} id={Statuses.TO_DO}>
+          기한: 추후
+        </Item>
+        <Item onClick={handleItemClick} id={Statuses.DONE}>
+          기한: 완료
+        </Item>
+        <Separator />
+        <Submenu label="프로젝트">
+          {categories.map((category) => (
+            <Item onClick={handleCategoryClick} id={`${category.id}`}>
+              {category.text}
+            </Item>
+          ))}
+        </Submenu>
+      </Menu>
     </ToDoLi>
   );
 }
